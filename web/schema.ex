40711c1d868461fd602bcf236c4678af62f49b44
Raincal <cyj94228@gmail.com>
2017-02-24 03:11:39 +0800

    CURD

--- web/schema.ex
+++ web/schema.ex
@@ -1,12 +1,12 @@
 defmodule PhoenixGraphqlExample.Schema do
   use Absinthe.Schema
   import_types PhoenixGraphqlExample.Schema.Types
- 
+
   query do
     field :posts, list_of(:post) do
       resolve &PhoenixGraphqlExample.PostResolver.all/2
     end
- 
+
     field :users, list_of(:user) do
       resolve &PhoenixGraphqlExample.UserResolver.all/2
     end
@@ -14,6 +14,35 @@ defmodule PhoenixGraphqlExample.Schema do
     field :user, type: :user do
       arg :id, non_null(:id)
       resolve &PhoenixGraphqlExample.UserResolver.find/2
+    end
+  end
+
+  input_object :update_post_params do
+    field :title, non_null(:string)
+    field :body, non_null(:string)
+    field :user_id, non_null(:integer)
+  end
+
+  mutation do
+    field :create_post, type: :post do
+      arg :title, non_null(:string)
+      arg :body, non_null(:string)
+      arg :user_id, non_null(:integer)
+
+      resolve &PhoenixGraphqlExample.PostResolver.create/2
+    end
+
+    field :update_post, type: :post do
+      arg :id, non_null(:integer)
+      arg :post, :update_post_params
+
+      resolve &PhoenixGraphqlExample.PostResolver.update/2
+    end
+
+    field :delete_post, type: :post do
+      arg :id, non_null(:integer)
+
+      resolve &PhoenixGraphqlExample.PostResolver.delete/2
     end
   end
 end
